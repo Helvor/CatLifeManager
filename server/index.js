a87aa7883e1ff12ae1e5738952097f515d16083b
Helvor <info@elveli.net>
2025-12-14 15:04:51 +0100

    Fix routing problem index.html

--- server/index.js
+++ server/index.js
@@ -39,6 +39,11 @@ app.listen(3000, '0.0.0.0', () => {
 	console.log("Server running on port 3000");
 });
 
+// Routes Default
+// ==========================================
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 // Routes API
 // ===========================================
